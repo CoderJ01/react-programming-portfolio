c7365cd77bd239a48bf6a6821dec5cfebcf2b143
Coder J <buildingguy@sbcglobal.net>
2022-04-02 18:08:07 -0700

    about page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Project from './components/Project';
 
 function App() {
   return (
     <div>
       <main>
         <Header></Header>
+        <Project></Project>
       </main>
     </div>
   );
---
+++ src/components/Project/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import avatar from '../../assets/images/avatar.png';
+
+function Project() {
+  return (
+    <section className="project">
+        <div id="about-me">
+            <h2>About</h2>
+            <img src={avatar} alt="" />
+            <p>
+                Hello World! My name is Joshua Jones. As of Saturday April 2, 2022, I am currently in the 
+                process of learning how to program via a coding bootcamp. So far, I have learned the skills needed to
+                become a competent full-stack developer. For the front-end, I've learned HTML, CSS, and JavaScript; and 
+                have used three types of APIs (Application Program Interfaces) - web storage, third-party, and server-side. 
+                For the back-end, I learned the fundamentals of Node.js, Object-Oriented programming, Express.js, SQL, 
+                Object-Relational Mapping (ORM), and Model-View-Controllers (MVC). To navigate to the webpages of 
+                my websites, click on the images under "Portfolio".
+            </p>
+        </div>
+    </section>
+  );
+}
+
+export default Project;
