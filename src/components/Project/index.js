581aa309a0ef31f37947c8e17cce82eea5e2d6db
Coder J <buildingguy@sbcglobal.net>
2022-04-03 21:06:07 -0700

    create resume page

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 // import About from '../About/index.js';
 // import Portfolio from '../Portfolio/index.js';
-import Contact from '../Contact/index.js';
+// import Contact from '../Contact/index.js';
+import Resume from '../Resume/index.js';
 
 function Project() {
   return (
     <section className="project">
         {/* <About></About> */}
         {/* <Portfolio></Portfolio> */}
-        <Contact></Contact>
+        {/* <Contact></Contact> */}
+        <Resume></Resume>
     </section>
   );
 }
---
+++ src/components/Resume/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import resume from '../../assets/documents/resume.jpg';
+
+function Resume () {
+    return (
+        <div id="resume">
+            <h2>Resume</h2>
+            <p>Download my<a href={resume} download>resume</a></p><br/>
+            <h3>Front-end Proficiencies</h3>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>jQuery</li>
+                <li>responsive design</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+            </ul><br/>
+            <h3>Back-end Proficiencies</h3>
+            <ul>
+                <li>APIs</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>mySQL, Sequelize</li>
+                <li>MongoDB, Mongoose</li>
+                <li>REST</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Resume;
