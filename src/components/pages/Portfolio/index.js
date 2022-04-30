f95a172f9fa29d43d49b27e328ec84e1053d9e7a
Coder J <buildingguy@sbcglobal.net>
2022-04-30 13:31:57 -0700

    reinitialize app

--- src/components/pages/Portfolio/index.js
+++ src/components/pages/Portfolio/index.js
@@ -62,6 +62,7 @@ function Portfolio() {
                         </a>
                     </div>
                 </div>
+                {/* random */}
                 <div className="pair-wrapper">
                     <div className="application">
                         <a href="https://national-parks-reviews.herokuapp.com/" rel="noreferrer" target="_blank">Nelp</a>
